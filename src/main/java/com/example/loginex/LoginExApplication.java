1aeb29b4d417685c671a7e31d59dd76f966dbd5f
jj99526 <jj99526@naver.com>
2023-03-10 21:36:04 +0900

    Initial commit

---
+++ src/main/java/com/example/loginex/LoginExApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginExApplication.class, args);
+    }
+
+}
